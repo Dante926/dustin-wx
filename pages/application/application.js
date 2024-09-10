0283ec46c59c669acb1dcbe3a56be4f3bb16757e
Dante <2802471732@qq.com>
2024-09-10 21:04:39 +0800

    ①实现普通用户申请租赁机器功能 ②调整租赁弹窗样式

--- pages/application/application.js
+++ pages/application/application.js
@@ -1,8 +1,9 @@
 const app = getApp();
+import {axios} from '../../utils/axios'
 Page({
   data: {
     showModal: false,
-    phoneNumber: '',
+    phone: '',
     deviceOptions: ['01', '02'], // 设备选项示例
     deviceIndex: 0,
     nickname: '',
@@ -18,7 +19,7 @@ Page({
     });
   },
 
-  // 蒙版
+  // 蒙版 关闭弹窗
   hideModal() {
     this.setData({
       showModal: false
@@ -46,36 +47,67 @@ Page({
     });
   },
 
-  // 提交租赁
+  // 提交租赁信息
   submitForm() {
     const {
-      phoneNumber,
+      phone,
       deviceOptions,
       deviceIndex,
       nickname
     } = this.data;
     const deviceNumber = deviceOptions[deviceIndex];
-    if (phoneNumber && deviceNumber && nickname) {
+    if (phone && deviceNumber && nickname) {
+      if(app.globalData.Mone < 100){ // 如果用户余额不足100元
+        wx.showToast({
+          title: '租赁机器需先支付100元，您的余额不足请先充值',
+          icon:'none'
+        })
+        return;
+      }else{
       // 这里可以添加表单提交的逻辑，例如调用 API 或存储数据
-      wx.showToast({
+      const params = {
+        phone:this.data.phone,
+        name:this.data.nickname,
+        manage_eq_id:Number(deviceIndex)+1,
+        pay_mone:this.data.amount
+      }
+      axios('/admin/application','POST',params)
+      .then(res=>{
+        wx.showModal({
+          title: '申请情况',
+          content: res.data.message,
+          complete: (res) => {
+            if (res.cancel) {
+              return
+            }
+            if (res.confirm) {
+              return
+            }
+          }
+        })
+      })
+      /* wx.showToast({
         title: '申请提交成功',
         icon: 'success'
       });
-      this.hideModal();
-    } else {
+      this.hideModal(); */
+      }
+    } else { // 有必填项未填
       wx.showToast({
         title: '请填写所有字段',
         icon: 'none'
       });
+      return;
     }
   },
 
-    // 申请租赁Ene----------------------------
+  // 申请租赁Ene----------------------------
 
   onShow() {
     console.log(app.globalData);
     this.setData({
-      power: app.globalData.power
+      power: app.globalData.power,
+      phone:app.globalData.UserPhone
     })
   }
 });
